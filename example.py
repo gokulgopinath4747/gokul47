5e58353eab90c9abc3301fc10f7e998efc693278
Solomon Hombiku <solomon.hombiku@gmail.com>
2019-12-23 23:37:14 +0100

    Added example file

--- example.py
+++ example.py
@@ -4,6 +4,9 @@
     # To take input from the user
     #num = int(input("Enter a number: "))
     factorial = 1
+
+
+    hgiycuutcvyibuhygvcyreytvfuygbinhuligyjrhyxuncr
     # check if the number is negative, positive or zero
     if num < 0:
        print("Sorry, factorial does not exist for negative numbers")
